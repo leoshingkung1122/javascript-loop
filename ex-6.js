506aa342de30a075666ba4bab79efae019bb3a47
kanyaweeN <40629441+kanyaweeN@users.noreply.github.com>
2025-07-21 14:01:19 +0700

    feat:ex1-6

--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,7 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
-
+for(let i = 0 ; i < companyName.length ; i++){
+    reversedCompanyName += companyName[(companyName.length-1)-i];
+}
 console.log(reversedCompanyName);
